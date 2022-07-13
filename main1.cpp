3f7a4e067874ab074d39e55fdede3716cec4619c
yashwanth <58801238+yashwanththati5@users.noreply.github.com>
2022-07-13 20:22:00 +0530

    Add files via upload

---
+++ main1.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nl cout<<"\n"
+void solve()
+{
+	
+	nl;
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	int choice = 1;
+	if(choice == 0)
+	{
+		solve();
+	}
+	else
+	{
+	int t; cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	}
+	return 0;
+}
