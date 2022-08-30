411744de837085d68e7ccdd31e85b1159732cd05
yashwanth <58801238+yashwanththati5@users.noreply.github.com>
2022-08-30 19:51:51 +0530

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 #define nl cout<<"\n";
 #define ll long long
-#define mod 1e+7
+#define mod 1000000007
 #define tc int t;cin>>t;while(t--)
 void solve()
 {
-	
+    
+    nl;
 }
 int main()
 {
-	ios_base::sync_with_stdio(false);
+  ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     bool ch = 1;
     if(!ch) solve();
