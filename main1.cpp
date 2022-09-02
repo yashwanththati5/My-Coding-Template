b25378598f161fd866f3a97c3265286107a0a031
yashwanth <58801238+yashwanththati5@users.noreply.github.com>
2022-09-02 15:16:32 +0530

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -4,9 +4,14 @@ using namespace std;
 #define ll long long
 #define mod 1000000007
 #define tc int t;cin>>t;while(t--)
+#define print(x) _print(x);
+template<class T> void _print(vector<T> v){for(auto i: v){cout<<i<<" ";} nl;}
+template<class T , class V> void _print(map<T , V> m){for(auto p: m){cout<<p.first<<" "<<p.second;}nl;}
+template<class T> void _print(set<T> s){for(auto p: s){cout<<p<<" ";}nl;}
+template<class T , class V> void _print(pair<T , V> p){for(auto x: p){cout<<x.first<<" "<<x.second; nl;}nl;}
 void solve()
 {
-    
+   
     nl;
 }
 int main()
