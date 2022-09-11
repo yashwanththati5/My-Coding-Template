b0adf8dbe644b3b263070cb673b89d3b51cd8438
yashwanth <58801238+yashwanththati5@users.noreply.github.com>
2022-09-11 22:49:17 +0530

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -5,22 +5,24 @@ using namespace std;
 #define mod 1000000007
 #define tc int t;cin>>t;while(t--)
 #define print(x) _print(x);
+ll admod(ll a , ll b , int m = 1000000007) {return (a % m + b % m) % m;}
+ll mmod(ll a , ll b , int m = 1000000007) {return (a % mod * b % mod) % mod;}
 template<class T> void _print(vector<T> v){for(auto i: v){cout<<i<<" ";} nl;}
 template<class T , class V> void _print(map<T , V> m){for(auto p: m){cout<<p.first<<" "<<p.second;}nl;}
 template<class T> void _print(set<T> s){for(auto p: s){cout<<p<<" ";}nl;}
 template<class T , class V> void _print(pair<T , V> p){for(auto x: p){cout<<x.first<<" "<<x.second; nl;}nl;}
 void solve()
-{
-   
-    nl;
+{ 
+  
+  nl;
 }
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    bool ch = 1;
-    if(!ch) solve();
-    else {tc solve();}
-
-    return 0;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  bool ch = 0;
+  if(!ch) solve();
+  else {tc solve();}
+      return 0;
 }
