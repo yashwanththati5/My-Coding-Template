b2b4de660382e73108a992415d461f608213f156
yashwanth <58801238+yashwanththati5@users.noreply.github.com>
2022-09-24 18:47:28 +0530

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -4,6 +4,8 @@ using namespace std;
 #define ll long long
 #define mod 1000000007
 #define tc int t;cin>>t;while(t--)
+#define max(v) *max_element((v).begin(), (v).end());
+#define min(v) *min_element((v).begin(), (v).end());
 #define print(x) _print(x);
 ll admod(ll a , ll b , int m = 1000000007) {return (a % m + b % m) % m;}
 ll mmod(ll a , ll b , int m = 1000000007) {return (a % m * b % m) % m;}
@@ -13,15 +15,17 @@ template<class T> void _print(set<T> s){for(auto p: s){cout<<p<<" ";}nl;}
 template<class T , class V> void _print(pair<T , V> p){for(auto x: p){cout<<x.first<<" "<<x.second; nl;}nl;}
 void solve()
 { 
-  nl;
+    
+    nl;
 }
+//---------------*******MAIN*******------------//
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
-  bool ch = 1;
+  bool ch = 0;
   if(!ch) solve();
-  else {tc solve();}
+  else {tc {solve();}}
       return 0;
 }
