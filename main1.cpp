23176003088a0150551813a85d61dfdcbccd91d4
yashwanth <58801238+yashwanththati5@users.noreply.github.com>
2022-10-03 12:23:09 +0530

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
-using namespace std;
+using namespace std;       
 #define nl cout<<"\n";
 #define ll long long
 #define mod 1000000007
@@ -7,30 +7,33 @@ using namespace std;
 #define maxv(v) *max_element((v).begin(), (v).end());
 #define minv(v) *min_element((v).begin(), (v).end());
 #define print(x) _print(x);
-ll admod(ll a , ll b , int m = 1000000007) {return (a % m + b % m) % m;}
+ll amod(ll a , ll b , int m = 1000000007) {return (a % m + b % m) % m;}
 ll mmod(ll a , ll b , int m = 1000000007) {return (a % m * b % m) % m;}
+ll smod(ll a , ll b , int m = 1000000007) {return (a % m - b % m + m) % m;}
+ll dmod(ll a , ll b , int m = 1000000007) {return (a % m * b ^ -1 % m) % m;}
 template<class T> void _print(vector<T> v){for(auto i: v){cout<<i<<" ";} nl;}
 template<class T , class V> void _print(map<T , V> m){for(auto p: m){cout<<p.first<<" "<<p.second;nl;}nl;}
 template<class T , class V> void _print(unordered_map<T , V> m){for(auto p: m){cout<<p.first<<" "<<p.second;nl;}nl;}
 template<class T> void _print(set<T> s){for(auto p: s){cout<<p<<" ";}nl;}
+template<class T> void _print(unordered_set<T> s){for(auto p: s){cout<<p<<" ";}nl;}
 template<class T , class V> void _print(pair<T , V> p){for(auto x: p){cout<<x.first<<" "<<x.second; nl;}nl;}
+//------- Options -----//
+bool ch = 1, cmp = 0;
+//---------------------//
+
 void solve()
 {
-
+  
   nl;
 }
+
 //---------------*******MAIN*******------------//
 int main()
 {
-
-  // #ifndef ONLINE_JUDGE 
-  // freopen("input.txt" , "r" , stdin);
-  // freopen("output.txt" , "w" , stdout);
-  // #endif
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
-  bool ch = 1;
+  if(cmp) {freopen("input.txt" , "r" , stdin); freopen("output.txt" , "w" , stdout);}
   if(!ch) solve();
   else {tc {solve();}}
       return 0;
